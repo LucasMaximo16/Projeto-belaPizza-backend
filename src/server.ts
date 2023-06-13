9e8ef07d52438bb1b867463f793a1a61d8bbd502
Lucas <lucasmaximodeandrade@gmail.com>
2023-06-13 00:49:20 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ io.on('connection', (socket) => {
 
 app.use(express.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(routes)
 
 
